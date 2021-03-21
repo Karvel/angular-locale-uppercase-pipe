7dc06b99859a97123978a4d864de377f84658d49
Elanna Grossman <elanna.grossman@gmail.com>
2021-03-21 13:50:52 -0700

    test(not-found): add test for `back()`

--- src/app/features/app/not-found/not-found.component.spec.ts
+++ src/app/features/app/not-found/not-found.component.spec.ts
@@ -1,3 +1,5 @@
+import { Location } from '@angular/common';
+
 import { createComponentFactory, Spectator } from '@ngneat/spectator';
 
 import { NotFoundComponent } from './not-found.component';
@@ -12,5 +14,13 @@ describe('[Integration] NotFoundComponent', () => {
 
   it('should create', () => {
     expect(spectator.component).toBeTruthy();
+  });
+
+  describe('back()', () => {
+    it('should call location.back', () => {
+      const spy = spyOn(spectator.inject(Location, true), 'back');
+      spectator.component.back();
+      expect(spy).toHaveBeenCalled();
+    });
   });
 });
