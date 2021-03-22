73f71f311e08d0c790d00d189c50b7e820deac3d
Elanna Grossman <elanna.grossman@gmail.com>
2021-03-21 22:54:38 -0700

    feat test(shared, test): add `LocaleUpperCasePipe` (#13)
    * feat(constants): add default language constant value
    * feat, test(shared): add and wire up `LocaleUpperCasePipe`
    * feat, test(test): add text and call pipe, mock dependency
    * feat(test): provide invalid argument to third example
    * feat(test): add labels to cards
    * feat(test): adjust positioning
    * feat(styles): add overrides
    * feat(test): use card header, style
    * refactor(pipes): apply linter fix

--- src/app/features/sandbox/test/test.component.spec.ts
+++ src/app/features/sandbox/test/test.component.spec.ts
@@ -1,14 +1,16 @@
 import { MatCardModule } from '@angular/material/card';
 
 import { createComponentFactory, Spectator } from '@ngneat/spectator';
-import { MockModule } from 'ng-mocks';
+import { MockModule, MockPipe } from 'ng-mocks';
 
+import { LocaleUpperCasePipe } from '@shared/pipes/locale-upper-case.pipe';
 import { TestComponent } from './test.component';
 
 describe('[Integration] TestComponent', () => {
   let spectator: Spectator<TestComponent>;
   const createComponent = createComponentFactory({
     component: TestComponent,
+    declarations: [MockPipe(LocaleUpperCasePipe)],
     imports: [MockModule(MatCardModule)],
   });
 
---
+++ src/app/infrastructure/shared/pipes/index.ts
@@ -0,0 +1,6 @@
+import { LocaleUpperCasePipe } from './locale-upper-case.pipe';
+
+/**
+ * Add pipes that do not need to be specifically referenced.
+ */
+export const pipes = [LocaleUpperCasePipe];
