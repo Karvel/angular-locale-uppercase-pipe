9fad35c8c723054d60b9fb483ddc73ea8107072d
Elanna Grossman <elanna.grossman@gmail.com>
2021-03-21 22:02:12 -0700

    feat, test(shared): add and wire up `LocaleUpperCasePipe`

--- src/app/infrastructure/shared/shared.module.ts
+++ src/app/infrastructure/shared/shared.module.ts
@@ -5,14 +5,21 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from './imports/material.module';
 
 import { components } from './components';
+import { pipes } from './pipes';
 
 /**
  * Connects modules, pipes, directives, components, and more that do not need to be singleton instances.
  */
 @NgModule({
-  declarations: [components],
+  declarations: [components, pipes],
   imports: [CommonModule, MaterialModule, ReactiveFormsModule],
-  exports: [CommonModule, MaterialModule, ReactiveFormsModule, components],
+  exports: [
+    CommonModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    components,
+    pipes,
+  ],
 })
 export class SharedModule {
   static forRoot(): { ngModule: typeof SharedModule } {
