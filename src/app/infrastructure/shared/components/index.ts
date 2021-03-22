6725c9fc502560cad05aa299062fb6334df748a0
Elanna Grossman <elanna.grossman@gmail.com>
2021-03-21 20:43:13 -0700

    feat(app, shared): add and wire up `FooterComponent`

---
+++ src/app/infrastructure/shared/components/index.ts
@@ -0,0 +1,6 @@
+import { FooterComponent } from './footer/footer.component';
+
+/**
+ * Add components that do not need to be specifically referenced.
+ */
+export const components = [FooterComponent];
